25a77176ea76b130a08eda5b716dbd22ef241abc
Mohit Gera <mohitgera5510@gmail.com>
2024-05-16 03:03:11 +0530

    completed

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,17 +1,17 @@
-// import { useAuthContext } from "../../context/AuthContext";
-// import { extractTime } from "../../utils/extractTime";
-// import useConversation from "../../zustand/useConversation";
+import { useAuthContext } from "../../context/AuthContext";
+import { extractTime } from "../../utils/extractTime";
+import useConversation from "../../zustand/useConversation";
 
 const Message = ({ message }) => {
-	// const { authUser } = useAuthContext();
-	// const { selectedConversation } = useConversation();
-	// const fromMe = message.senderId === authUser._id;
-	// const formattedTime = extractTime(message.createdAt);
-	// const chatClassName = fromMe ? "chat-end" : "chat-start";
-	// const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
-	// const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+	const { authUser } = useAuthContext();
+	const { selectedConversation } = useConversation();
+	const fromMe = message.senderId === authUser._id;
+	const formattedTime = extractTime(message.createdAt);
+	const chatClassName = fromMe ? "chat-end" : "chat-start";
+	const profilePic = fromMe ? authUser.profilePic : selectedConversation?.profilePic;
+	const bubbleBgColor = fromMe ? "bg-blue-500" : "";
 
-	// const shakeClass = message.shouldShake ? "shake" : "";
+	const shakeClass = message.shouldShake ? "shake" : "";
 
 	return (
 		<div className={`chat ${chatClassName}`}>
